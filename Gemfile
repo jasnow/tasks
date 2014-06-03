6ecfc2e4c7397876aadab64f1176637f8abec711
Al Snow <jasnow@hotmail.com>
2014-06-03 08:01:21 -0400

    Rebuilt Gemfile.lock file; Upgraded rspec-rails to 3.0.0

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'jquery-rails'
 
 group :test, :development do
 #  gem 'turn'
-  gem 'rspec-rails', '2.99.0'
+  gem 'rspec-rails'
   gem 'capybara'
   gem 'rb-inotify' if /linux/ =~ RUBY_PLATFORM
   gem 'launchy'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require File.expand_path("../../config/environment", __FILE__)
 gem "minitest"
 require 'minitest/autorun'
 require 'rspec/rails'
-require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
