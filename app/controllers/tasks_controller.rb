f501be69b2f0cc7f002e3242a7f10e1230f26190
Al Snow <jasnow@hotmail.com>
2018-03-04 10:44:05 -0500

    Changed 'update_attributes' to 'update'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,7 +16,7 @@ class TasksController < ApplicationController
 
   def update
     task = Task.find params[:id]
-    if task.update_attributes(task_params)
+    if task.update(task_params)
       redirect_to tasks_path,  :notice => 'Your tasks successfully been updated'
     else
       redirect_to(request.referer,
