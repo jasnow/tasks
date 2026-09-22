3ac6f530b9facfb66bced60f608b7e7360540788
Al Snow <43523+jasnow@users.noreply.github.com>
2026-09-22 11:33:44 -0400

    Upgraded gem(s): [puts,regexp_parser,sexp_processor]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 require 'simplecov'
 if ENV["COVERAGE"]
   SimpleCov.start 'rails' do
-    puts "Adding simplecov"
+    puts 'Adding simplecov'
     enable_coverage :branch
     primary_coverage :branch
   end
 end
-
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
